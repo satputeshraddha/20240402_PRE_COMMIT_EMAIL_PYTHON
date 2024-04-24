ea4334e02d1fe036a5e10f5d5ac868ea45e47ac3
satputeshraddha <satputeshraddha123@dnyanyog.org>
2024-04-24 17:20:46 +0530

    feat:implement pre-commit check for python

---
+++ product.java
@@ -0,0 +1,12 @@
+public class Product {
+
+  public String productName;
+  public int productQuantity;
+  public double perProductPrice;
+
+  public Product(String productName, double perProductPrice, int productQuantity) {
+    this.productName = productName;
+    this.productQuantity = productQuantity;
+    this.perProductPrice = perProductPrice;
+  }
+}
